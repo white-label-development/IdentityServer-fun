810efd6ff9d5f795bf8c6f68f38edde1757ba5e0
Neil Thompson <neil@white-label-development.co.uk>
2020-04-29 10:00:55 +0100

    complete quickstart 1

---
+++ Quickstarts/src/Api/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // adds the authentication services to DI and configures Bearer as the default scheme.
+            services.AddAuthentication("Bearer")
+            .AddJwtBearer("Bearer", options =>
+            {
+                options.Authority = "https://localhost:5001";
+                options.RequireHttpsMetadata = false;
+
+                options.Audience = "api1";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication(); // who
+            app.UseAuthorization();  // permissions
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # IdentityServer-fun
+
 quickstarts zero to hero (hopefully)
+
+add templates `dotnet new -i IdentityServer4.Templates`
+
+## Protecting an API using Client Credentials
+https://identityserver4.readthedocs.io/en/latest/quickstarts/1_client_credentials.html
+
+```
+dotnet new is4empty -n IdentityServer
+// added a sln
+```
+
+in config added an API ` new ApiResource("api1", "My API")` and a Client (non interactive user, use just the embedded clientid/secret for authentication)
+
+run and go to disco doc https://localhost:5001/.well-known/openid-configuration
+
+Added an API on :5002. added IdentityController. `install-package Microsoft.AspNetCore.Authentication.JwtBearer`. 
+
+Register services and add middleware via Startup.cs
