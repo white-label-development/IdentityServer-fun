631e8cb969d289d398707bfbe4ab0a0f35a72ec2
Neil Thompson <neil@white-label-development.co.uk>
2020-04-29 08:06:05 +0100

    add reference app

---
+++ IdentityServerAndApi-master/IdentityServerHost/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi
+{
+    public class TestController : ControllerBase
+    {
+        [Route("test")]
+        [Authorize(IdentityServerConstants.LocalApi.PolicyName)]
+        public IActionResult Get()
+        {
+            var claims = User.Claims.Select(c => new { c.Type, c.Value }).ToArray();
+            return Ok(new { message = "Hello API", claims });
+        }
+    }
+}
